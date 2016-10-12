2151ebf329daa9a39b231da8da0fb0762cc63981
Jason Butwell <jasonbutwell@gmail.com>
2016-10-12 03:28:24 +0100

    Implemented the click listener for a long press on the Google Map to create a map marker at that location.

--- app/src/main/java/com/jasonbutwell/memorableplaces/yourPlaces.java
+++ app/src/main/java/com/jasonbutwell/memorableplaces/yourPlaces.java
@@ -1,33 +1,53 @@
 package com.jasonbutwell.memorableplaces;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.Window;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class yourPlaces extends AppCompatActivity implements OnMapReadyCallback {
+public class yourPlaces extends AppCompatActivity implements GoogleMap.OnMapLongClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
-    public android.support.v7.app.ActionBar actionBar;
+    public ActionBar actionBar;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                this.finish();
+                this.finish();      // If home button closed then finish this activity
                 return true;
             default:
-                return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected(item);   // default
         }
+    }
+
+    @Override
+    public void onMapLongClick( LatLng point ) {
+        mMap.addMarker(( new MarkerOptions()
+        .position(point)
+        .title("You are here")
+        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))));
     }
 
     @Override
@@ -41,7 +61,7 @@ public class yourPlaces extends AppCompatActivity implements OnMapReadyCallback 
         // Enable the back button on the action bar
 
         actionBar = getSupportActionBar();
-        actionBar.setDisplayShowHomeEnabled( true );
+        actionBar.setDisplayShowHomeEnabled(true);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -51,7 +71,11 @@ public class yourPlaces extends AppCompatActivity implements OnMapReadyCallback 
         // get the data we passed in from earlier from the first intent.
 
         Intent intent = getIntent();
-        Log.i("locationInfo",Integer.toString(intent.getIntExtra("locationInfo",-1)));
+        Log.i("locationInfo", Integer.toString(intent.getIntExtra("locationInfo", -1)));
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -71,5 +95,44 @@ public class yourPlaces extends AppCompatActivity implements OnMapReadyCallback 
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Add the long click listener
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("yourPlaces Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
