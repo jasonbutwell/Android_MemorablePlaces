7d30576b3736c13a2431a18d3e83c3d337e72ab5
Jason Butwell <jasonbutwell@gmail.com>
2016-10-12 03:15:38 +0100

    Changed the map activity to extend AppCompatActivity. Implemented code to allow the action bar to have a functional back button on the action bar which when clicked closes the map activity. The MainActivity now also calls the Google Maps Activity.

--- app/src/main/java/com/jasonbutwell/memorableplaces/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/memorableplaces/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jasonbutwell.memorableplaces;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
         listView.setAdapter(arrayAdapter);
 
-        
+        // call the map activity
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), yourPlaces.class);
+                intent.putExtra("locationInfo", position);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/jasonbutwell/memorableplaces/yourPlaces.java
@@ -0,0 +1,75 @@
+package com.jasonbutwell.memorableplaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class yourPlaces extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    public android.support.v7.app.ActionBar actionBar;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+
+        setContentView(R.layout.activity_your_places);
+
+        // Enable the back button on the action bar
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled( true );
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // get the data we passed in from earlier from the first intent.
+
+        Intent intent = getIntent();
+        Log.i("locationInfo",Integer.toString(intent.getIntExtra("locationInfo",-1)));
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
