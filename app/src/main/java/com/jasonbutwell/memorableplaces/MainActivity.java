f9788fd3f1581ff3e04a3366deedb0cf5be96ec2
Jason Butwell <jasonbutwell@gmail.com>
2016-10-12 02:33:30 +0100

    Created the listview for the first activity.

--- app/src/main/java/com/jasonbutwell/memorableplaces/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/memorableplaces/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jasonbutwell.memorableplaces;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById( R.id.listView );
+
+        final ArrayList<String> places = new ArrayList<>();
+
+        places.add("Add a new place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+        listView.setAdapter(arrayAdapter);
+
+        
     }
 }
